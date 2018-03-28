42d40db494f503eb0a4f74bd8739dd7484005358
Katricia Herring <kherring@nd.edu>
2018-03-28 17:14:58 -0400

    it works~

--- kat/salesdb_kat.sql
+++ kat/salesdb_kat.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE PACKAGE salesdb_kat is
   --- Get total profit, transactions, total per transaction
-  function sp_tot_profit(salpers_id salesperson.salpers_id%type)
+  function sp_tot_profit(salpers_id_f salesperson.salpers_id%type)
     return float;
 END salesdb_kat;
 /
